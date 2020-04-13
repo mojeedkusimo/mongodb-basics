4065576ac715a799c59d9fb28405edd1fda9a18a
Mojeedkush <mkusimo90@gmail.com>
2020-04-13 02:38:43 +0100

    StartNG node task 3 completed

--- findInterns.js
+++ findInterns.js
@@ -1,25 +1,25 @@
 let findInterns = function (myDB) {
     let query = {rating : 7};
 
-    // myDB.collection('myMovies').findOne({}, function (err,result) {
-    //     if (err) throw err;
-    //     console.log( result);
-    // });
-
-    myDB.collection('myMovies').find({}).toArray(function (err,result) {
+    myDB.collection('myMovies').findOne({}, function (err,result) {
         if (err) throw err;
-        console.log( result );
+        console.log( result);
     });
 
-    // myDB.collection('myMovies').find(query).toArray(function (err,result) {
+    // myDB.collection('myMovies').find({}).toArray(function (err,result) {
     //     if (err) throw err;
-    //     console.log(result);
+    //     console.log( result );
     // });
 
-    // myDB.collection('myMovies').find({}, {projection: { movie : 1 }}).toArray(function (err,result) {
-    //     if (err) throw err;
-    //     console.log(result);
-    // });
+    myDB.collection('myMovies').find(query).toArray(function (err,result) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+    myDB.collection('myMovies').find({}, {projection: { _id: 0, movie : 1 }}).toArray(function (err,result) {
+        if (err) throw err;
+        console.log(result);
+    });
 }
 
 module.exports = findInterns;
